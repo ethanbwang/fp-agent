80fd3ffda2e1b5d47649e6e689f6a0a2df66627e
Ethan Wang <ethanbwang@gmail.com>
2026-08-27 20:20:43 -0700

    EventType for num interactions experiment

--- classifier_training/src/classifier_training/types.py
+++ classifier_training/src/classifier_training/types.py
@@ -1,6 +1,13 @@
 from enum import Enum
 
 
+class EventType(Enum):
+    KEYBOARD = "keyboard"
+    MOUSE = "mouse"
+    SCROLL = "scroll"
+    ALL = "all"
+
+
 class FeatureType(Enum):
     BROWSER = 1
     BEHAVIORAL = 2
